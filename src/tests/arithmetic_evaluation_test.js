97c173c8ebac443e299a97cf596463a5814d040e
Arkadiusz Wegrzyn <arek.wegrzynw@gmail.com>
2020-03-30 10:03:01 +0200

    more tests added

--- src/tests/arithmetic_evaluation_test.js
+++ src/tests/arithmetic_evaluation_test.js
@@ -8,6 +8,39 @@ class ArithmeticEvaluationTest extends TestCase {
       Assert.equal(stdout.trim(), "7");
     })
   }
+
+  testMultipleAdditions() {
+    super.executeCode("print(2+5 + 10 +17 +100)", (err, stdout, stderr) => {
+      Assert.equal(stderr, "");
+      Assert.equal(stdout.trim(), "134");
+    })
+  }
+
+  testMultiplication() {
+    super.executeCode("print(2*5)", (err, stdout, stderr) => {
+      Assert.equal(stderr, "");
+      Assert.equal(stdout.trim(), "10");
+    })
+  }
+
+  testMultiMultiplication() {
+    super.executeCode("print(2*5 * 100)", (err, stdout, stderr) => {
+      Assert.equal(stderr, "");
+      Assert.equal(stdout.trim(), "1000");
+    })
+  }
+
+  testOrderOfOperations() {
+    super.executeCode("print(2+5 * 100)", (err, stdout, stderr) => {
+      Assert.equal(stderr, "");
+      Assert.equal(stdout.trim(), "502");
+    })
+
+    super.executeCode("print(2*7 + 5*100 / 2 - 8)", (err, stdout, stderr) => {
+      Assert.equal(stderr, "");
+      Assert.equal(stdout.trim(), "256");
+    })
+  }
 };
 
-ArtihmeticEvaluationTest.prototype.run()
+ArithmeticEvaluationTest.prototype.run()
